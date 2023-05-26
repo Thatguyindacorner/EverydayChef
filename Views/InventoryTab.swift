9205cb67030c24c5bf6a8ed0e44b856e34b80905
Thatguyindacorner <45794181+Thatguyindacorner@users.noreply.github.com>
2023-05-26 19:10:57 -0400

    EC-15 InventoryTab Toolbar Created
    ShoppingListView file created and linked from InventoryTab Toolbar
    
    Menu option created on Toolbar to swap StorageLocation (fridge, pantry, bar)

--- Views/InventoryTab.swift
+++ Views/InventoryTab.swift
@@ -7,10 +7,76 @@
 
 import SwiftUI
 
+enum StorageLocation: String{
+    case fridge = "Fridge"
+    case pantry = "Pantry"
+    case bar = "Bar"
+}
+
 struct InventoryTab: View {
+    
+    @State var currentStorageType: StorageLocation = .fridge
+    
+    @State var showSettings: Bool = false
+    @State var inShoppingList: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            VStack{
+                NavigationLink(destination: ShoppingListView(), isActive: $inShoppingList){}
+                //fridge content
+                //5 shelves
+                Text("Hello World")
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading){
+                    Button(action:{
+                        //profile, settings, loggout, etc.
+                        //custom sidebar sliding from left
+                    }){
+                        Image(systemName: "person.circle")
+                    }
+                }
+                
+                ToolbarItem(placement: .principal){
+                    Menu {
+                        //function won't render option if it's already choosen
+                        StorageOption(.fridge)
+                        StorageOption(.pantry)
+                        StorageOption(.bar)
+                        
+                    } label: {
+                        HStack{
+                            Text(currentStorageType.rawValue)
+                            Image(systemName: "chevron.down")
+                        }
+                    }
+
+                }
+
+                ToolbarItem(placement: .navigationBarTrailing){
+                    Button(action:{
+                        //shopping list
+                        inShoppingList = true
+                    }){
+                        Image(systemName: "cart")
+                    }
+                }
+            }
+        }
     }
+    
+    @ViewBuilder
+    func StorageOption(_ loc: StorageLocation) -> some View{
+        if loc != currentStorageType{
+            Button(action:{
+                currentStorageType = loc
+            }){
+                Text(loc.rawValue)
+            }
+        }
+    }
+    
 }
 
 struct InventoryTab_Previews: PreviewProvider {
