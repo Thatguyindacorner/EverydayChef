3def02103386b32e65a17587c837e5ff9b0f9272
Thatguyindacorner <45794181+Thatguyindacorner@users.noreply.github.com>
2023-05-26 00:15:38 -0400

    EC-4 MVC Project Folders added
    Tab View files create and put in Views folder

---
+++ Views/HistoryTab.swift
@@ -0,0 +1,20 @@
+//
+//  HistoryTab.swift
+//  EverydayChef
+//
+//  Created by Alex Olechnowicz on 2023-05-26.
+//
+
+import SwiftUI
+
+struct HistoryTab: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HistoryTab_Previews: PreviewProvider {
+    static var previews: some View {
+        HistoryTab()
+    }
+}
