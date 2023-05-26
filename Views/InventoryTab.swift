66b8ae2b63797c169608d8ba0efb0e07d44ce349
Thatguyindacorner <45794181+Thatguyindacorner@users.noreply.github.com>
2023-05-26 00:15:38 -0400

    EC-4 MVC Project Folders added
    Tab View files create and put in Views folder

---
+++ Views/InventoryTab.swift
@@ -0,0 +1,20 @@
+//
+//  InventoryTab.swift
+//  EverydayChef
+//
+//  Created by Alex Olechnowicz on 2023-05-26.
+//
+
+import SwiftUI
+
+struct InventoryTab: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InventoryTab_Previews: PreviewProvider {
+    static var previews: some View {
+        InventoryTab()
+    }
+}
