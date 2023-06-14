d79504a1743a2c4584f2140c15dee5580560547f
Thatguyindacorner <45794181+Thatguyindacorner@users.noreply.github.com>
2023-06-14 17:39:30 -0400

    EC-27 + EC-3 Splash Screen Made
    Flow of app starts with a Splash Screen and check to see if your phone has a stored account then either takes you to the StartView or the ContentView

--- EverydayChef/EverydayChefApp.swift
+++ EverydayChef/EverydayChefApp.swift
@@ -25,11 +25,80 @@ struct EverydayChefApp: App {
     
     var session: SessionData = SessionData.shared
     
+    @State var splashTimeOver: Bool = false
+    
     var body: some Scene {
-        WindowGroup {
-            //ContentView()
-            StartView().environmentObject(session)
-                //.navigationBarHidden(true)
+        
+        
+        WindowGroup{
+            Group {
+                if splashTimeOver {
+                    StartView(authenticated: AuthController().authentication.currentUser != nil).environmentObject(session)
+                } else {
+                    SplashView()
+                }
+            }.onAppear{
+                //3.5 seconds of splash then goto login (if account saved, skip login and go straigt to account)
+                Task{
+                    DispatchQueue.main.asyncAfter(deadline: .now() + Double(3.5)){
+                        self.splashTimeOver = true
+                    }
+                }
+                
+                //1 check if connected to the internet
+                //2 check if account is stored on phone
+                //3 redirect
+                
+                //redirect to account
+                if true{
+                    
+                }
+                //redirect to StartView
+                else{
+                    
+                }
+            }
         }
+        
+        //        WindowGroup(id: "StartView", for: Bool.self){ $skipLogin in
+        //            if skipLogin == nil{
+        //                StartView(authenticated: false).environmentObject(session)
+        //            }
+        //            else{
+        //                StartView(authenticated: skipLogin!).environmentObject(session)
+        //            }
+        //        }
+        
+    }
+    
+}
+
+struct SplashView: View{
+    
+    let widthScreen = UIScreen.main.bounds.size.width - 25
+    let heightScreen = UIScreen.main.bounds.size.height
+    
+    //var splashTimeOver: Bool = false
+    
+    //var session: SessionData = SessionData.shared
+    
+    //@Environment(\.openWindow) private var openWindow
+    
+    var body: some View{
+        
+//        Group{
+//            if splashTimeOver{
+//                StartView(authenticated: AuthController().authentication.currentUser != nil).environmentObject(session)
+//            }
+//        }
+        
+        VStack{
+            Image("HomeImage").resizable().frame(width: widthScreen / 1.5, height: heightScreen / 3.5)
+            HStack{
+                Text("Preping the Kitchen ") //text will change periodically
+                ProgressView()
+            }
+        }
+        
     }
 }
--- EverydayChef/Views/StartView.swift
+++ EverydayChef/Views/StartView.swift
@@ -12,7 +12,7 @@ struct StartView: View {
     let widthScreen = UIScreen.main.bounds.size.width - 25
     let heightScreen = UIScreen.main.bounds.size.height
     
-    @State var authenticated: Bool = false
+    @State var authenticated: Bool
     
     let authHelper: AuthController = AuthController()
     
@@ -20,90 +20,99 @@ struct StartView: View {
     
     var body: some View {
         
-        let _ = Binding<Bool> {
-            return authHelper.user != nil
-        } set: {
-            self.authenticated = $0
-        }
+//        let _ = Binding<Bool> {
+//            return authHelper.user != nil
+//        } set: {
+//            self.authenticated = $0
+//        }
 
         
         NavigationView{
             
-            VStack{
-                
-                HStack{
-                    Image("HomeImage").resizable()
-                }.frame(width: widthScreen, height: heightScreen / 3.5)
-                    .border(.black)
-                HStack(alignment: .bottom, spacing: 1){
-                    Text("Everyday Chef").font(.largeTitle).bold()
-                    Text("©").bold()
-                }.frame(width: widthScreen, height: heightScreen / 8)
-                    .border(.black)
-                HStack{
-                    Button(action:{
-                        Task{
-                            if await authHelper.anonymousAuth(){
-                                self.authenticated = true
-                        
-                            }
-                            else{
-                                //error
-                            }
-                        }
-                        
-                    }){
-                        Text("Get Started")
-                            .font(.title)
-                            .bold()
-                            .padding(10)
-                            .frame(width: 200).border(.blue)
-                            .background()
-                    }
-                }.frame(width: widthScreen, height: heightScreen / 8)
-                    .border(.black)
+            if !authenticated{
                 VStack{
                     
-                    Button(action:{
+                    HStack{
+                        Image("HomeImage").resizable()
+                    }.frame(width: widthScreen, height: heightScreen / 3.5)
+                        .border(.black)
+                    HStack(alignment: .bottom, spacing: 1){
+                        Text("Everyday Chef").font(.largeTitle).bold()
+                        Text("©").bold()
+                    }.frame(width: widthScreen, height: heightScreen / 8)
+                        .border(.black)
+                    HStack{
+                        Button(action:{
+                            Task{
+                                if await authHelper.anonymousAuth(){
+                                    self.authenticated = true
+                            
+                                }
+                                else{
+                                    //error
+                                }
+                            }
+                            
+                        }){
+                            Text("Get Started")
+                                .font(.title)
+                                .bold()
+                                .padding(10)
+                                .frame(width: 200).border(.blue)
+                                .background()
+                        }
+                    }.frame(width: widthScreen, height: heightScreen / 8)
+                        .border(.black)
+                    VStack{
                         
-                    }){
-                        Text("Sign In")
-                            .font(.title2)
-                            .bold()
-                            .padding(10)
-                            .frame(width: 200).border(.blue)
-                    }
-                    Button(action:{
+                        Button(action:{
+                            
+                        }){
+                            Text("Sign In")
+                                .font(.title2)
+                                .bold()
+                                .padding(10)
+                                .frame(width: 200).border(.blue)
+                        }
+                        Button(action:{
+                            
+                        }){
+                            Text("Sign Up")
+                                .font(.title2)
+                                .bold()
+                                .padding(10)
+                                .frame(width: 200).border(.blue)
+                        }
+                    }.frame(width: widthScreen, height: heightScreen / 5)
+                        .border(.black)
+                    NavigationLink(destination: ContentView().environmentObject(session), isActive: $authenticated){}
                         
-                    }){
-                        Text("Sign Up")
-                            .font(.title2)
-                            .bold()
-                            .padding(10)
-                            .frame(width: 200).border(.blue)
-                    }
-                }.frame(width: widthScreen, height: heightScreen / 5)
-                    .border(.black)
-                NavigationLink(destination: ContentView().environmentObject(session),isActive: $authenticated){}
-                    
+                }
+//                .onAppear{
+//                    //stored user
+//                    if session.loggedInUser != nil {
+//                        self.authenticated = true
+//
+//                    }
+//                }
+                .navigationBarHidden(true)
+            }
+            else{
+                VStack{
+                    NavigationLink(destination: ContentView().environmentObject(session), isActive: $authenticated){}
+                }
+                .navigationBarHidden(true)
             }
             
-            .onAppear{
-                //stored user
-                if session.loggedInUser != nil {
-                    self.authenticated = true
-                    
-                }
-            }
-            .navigationBarHidden(true)
+            
         }
         
         //Spacer()
     }
 }
 
-struct StartView_Previews: PreviewProvider {
-    static var previews: some View {
-        StartView()
-    }
-}
+//struct StartView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        StartView()
+//    }
+//}
