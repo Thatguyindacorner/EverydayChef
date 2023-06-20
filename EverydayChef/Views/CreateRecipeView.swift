2c3ef3c4d87b4fb6bdf60593ecbc128300f5d797
Ameya-Joshi79 <45701553+Ameya-Joshi79@users.noreply.github.com>
2023-06-19 22:18:57 -0400

    EC-49-create-recipe

--- EverydayChef/Views/CreateRecipeView.swift
+++ EverydayChef/Views/CreateRecipeView.swift
@@ -11,6 +11,12 @@ struct CreateRecipeView: View {
     
     @State private var cuisine:String = ""
     
+    @State private var ingredients:String = ""
+    
+    @State private var recipeName:String = ""
+    
+    @State private var directions:String = ""
+    
     var body: some View {
         if #available(iOS 16.0, *) {
             ScrollView{
@@ -18,11 +24,33 @@ struct CreateRecipeView: View {
                     Text("Create Recipe")
                         .font(.system(size: 24).bold())
                     
-                    TextField("Enter Cuisine", text: $cuisine, axis: .vertical)
-                        .textFieldStyle(.roundedBorder)
-                        .lineLimit(3)
+                    Group{
+                        TextField("Enter Recipe Name", text: $recipeName, axis: .vertical)
+                            .modifier(RecipeTFModifiers(paddingValue: 20.0, lineLimitVal: 5))
+                        
+                        TextField("Enter Cuisine", text: $cuisine, axis: .vertical)
+                            .modifier(RecipeTFModifiers(paddingValue: 15.0, lineLimitVal: 5))
+                        
+                        TextField("Enter Ingredients e.g. sugar, eggs", text: $ingredients)
+                            .modifier(RecipeTFModifiers(paddingValue: 20.0, lineLimitVal: 5))
+                        
+                        TextField("Enter Directions", text: $directions)
+                            .modifier(RecipeTFModifiers(paddingValue: 30, lineLimitVal: 5))
+                            
+                        
+                    }//Group
+                    .padding(.top, 10)
                     
-                    
+                    Button(action:{
+                        print("Save Data to Firestore for this user")
+                    }){
+                        Text("Save Recipe")
+                            .padding(10)
+                            .background(.blue)
+                            .foregroundColor(.white)
+                            .bold()
+                            .cornerRadius(10)
+                    }
                     
                 }//VStack
                 .padding()
@@ -30,6 +58,50 @@ struct CreateRecipeView: View {
             }//ScrollView
         }
     }//body
+}
+
+struct RecipeTFModifiers:ViewModifier{
+    
+    let paddingValue:CGFloat
+    
+    let lineLimitVal:Int
+    
+    
+    init(paddingValue: CGFloat, lineLimitVal:Int) {
+        self.paddingValue = paddingValue
+        self.lineLimitVal = lineLimitVal
+    }
+    
+    func body(content: Content) -> some View {
+        content
+            .textFieldStyle(CustomTextFieldStyle(setPadding: paddingValue))
+            .lineLimit(self.lineLimitVal)
+            .padding(.bottom, 10)
+//            .textFieldStyle(.roundedBorder)
+//            .frame(height: 48)
+//            .lineLimit(5)
+//            .padding(.bottom, 10)
+    }
+    
+}
+
+public struct CustomTextFieldStyle : TextFieldStyle {
+    
+    let setPadding:CGFloat
+    
+    init(setPadding: CGFloat) {
+        self.setPadding = setPadding
+    }
+    
+    public func _body(configuration: TextField<Self._Label>) -> some View {
+        configuration
+        //.font(.largeTitle) // set the inner Text Field Font
+            .padding(setPadding) // Set the inner Text Field Padding
+        //Give it some style
+            .background(
+                RoundedRectangle(cornerRadius: 5)
+                    .strokeBorder(Color.primary.opacity(0.5), lineWidth: 0.5))
+    }
 }
 
 struct CreateRecipeView_Previews: PreviewProvider {
